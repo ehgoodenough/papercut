4c864d5ff230a0fb37c5cbf4f439bb9ce99e74eb
skylarjhdownes <skylarjhdownes@gmail.com>
2015-10-11 11:52:19 -0700

    futzing

--- source/scripts/utilities/FindNearestPlayerProjectile.js
+++ source/scripts/utilities/FindNearestPlayerProjectile.js
@@ -4,14 +4,16 @@ var findNearestPlayerProjectile = function(point) {
 
     var nearestPlayerProjectile = {}
     var closest = 99999
-    for (var projectile in window.game.ninjastars){
-        if (projectile.type === "ninjastar"){ 
+    for (var projectile in window.game.projectiles){
+        if (projectile.target === "monster"){ 
             var distance = getDistanceBetweenPoints({x: projectile.x y: projectile.y }, {x: point.x y: point.y})
             if (distance < closest){
                 nearestPlayerProjectile = projectile
+                closest = distance
             }
         }
     }
+    return nearestPlayerProjectile
 }
 
 module.exports = findNearestPlayerProjectile
